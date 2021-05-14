e0a4a1fe1b99f327cdd86b773d6c6b13113495e5
Katie Durand <khd25@cornell.edu>
2021-05-14 13:46:19 -0500

    solve cyclically sorted array

--- Scenario 2/Column 2/11.3.js
+++ Scenario 2/Column 2/11.3.js
@@ -1,1 +1,29 @@
-//
+/*
+
+EPI 11.3 - SEARCH A CYCLICALLY SORTED ARRAY
+
+cyclically sorter means that the whole thing is sorted if you shift the whole thing over a certain number of spaces. ie it has a pivot point that if you cut off the part before and tack it on the end, it is totally sorted.
+
+this is basically "do binary search to find the pivot point (which is the min of array)"
+and return the idx of that pivot point
+
+*/
+
+function cyclicallySortedArrSearch(arr) {
+  let start = 0;
+  let end = arr.length - 1;
+  let mid;
+  while (start < end) {
+    mid = start + Math.floor((end - start) / 2);
+    if (arr[mid] > arr[end]) {
+      start = mid + 1;
+    } else {
+      end = mid;
+    }
+  }
+  return start;
+}
+
+let test = [5, 6, 7, 1, 2, 3, 4];
+
+console.log('idx = ', cyclicallySortedArrSearch(test)); // expect idx of 3
