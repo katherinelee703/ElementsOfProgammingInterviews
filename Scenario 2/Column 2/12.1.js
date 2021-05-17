e1744538cea73ada0e31e521d044b22add3f5474
Katie Durand <khd25@cornell.edu>
2021-05-17 10:44:03 -0500

    solve palindromic permutations

--- Scenario 2/Column 2/12.1.js
+++ Scenario 2/Column 2/12.1.js
@@ -1,1 +1,30 @@
-//
+/*
+
+EPI 12.1 - Palindromic Permutations
+
+write a function that tests whether a given string (word) can be "arranged" into a palindrome
+
+for example:
+level is already a palindrome
+edified can be turned into deified
+both should return true
+
+* this is a hash tables/set problem
+
+*/
+
+function palPerms(str) {
+  let oddFreq = new Set();
+  for (let i = 0; i < str.length; i++) {
+    if (oddFreq.has(str[i])) {
+      oddFreq.delete(str[i]);
+    } else {
+      oddFreq.add(str[i]);
+    }
+  }
+  return oddFreq.size <= 1;
+}
+
+console.log(palPerms('level')); // expect true
+console.log(palPerms('edified')); // expect true
+console.log(palPerms('racer')); // expect false
