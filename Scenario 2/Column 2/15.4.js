804ab9de0bcdd895517789733a56daa15de1815a
Katie Durand <khd25@cornell.edu>
2021-05-24 10:44:32 -0500

    solve generate permutations

--- Scenario 2/Column 2/15.4.js
+++ Scenario 2/Column 2/15.4.js
@@ -1,1 +1,31 @@
-//
+/*
+
+EPI 15.4 - GENERATE PERMUTATIONS 
+
+assume no duplicate elements
+
+*/
+
+let str = ['d', 'o', 'g', 'e'];
+let str2 = ['c', 'a', 't'];
+
+function perms(arr) {
+  const result = [];
+  backtrack(arr, result);
+  return result;
+}
+
+function backtrack(arr, result, n = 0) {
+  if (n === arr.length - 1) {
+    result.push(arr.slice(0));
+    return;
+  }
+  for (let i = n; i < arr.length; i++) {
+    [arr[i], arr[n]] = [arr[n], arr[i]];
+    backtrack(arr, result, n + 1);
+    [arr[i], arr[n]] = [arr[n], arr[i]];
+  }
+}
+
+console.log(perms(str)); // expect 24 elements
+console.log(perms(str2)); // expect 6 elements
